e8dfb5480242e2100089a8938dc7d279b124215e
Vitaly <vitaliy.zayakin@gmail.com>
2024-02-05 17:31:19 +0500

    feat: update add method to throw invalid input error on wrong data (e.g. path is not in current dir)

--- src/services/fs-service.js
+++ src/services/fs-service.js
@@ -1,11 +1,18 @@
-import { resolve } from 'node:path';
+import { resolve, parse } from 'node:path';
 import { writeFile } from 'node:fs/promises';
-import { OperationFailedError } from '../utils/error/index.js';
+import { InvalidInputError, OperationFailedError } from '../utils/error/index.js';
 
 export class FsService {
   async add([path]) {
+    const filePath = resolve(path);
+    const fileDir = parse(filePath).dir;
+
+    if (process.cwd() !== fileDir) {
+      throw new InvalidInputError();
+    }
+
     try {
-      await writeFile(resolve(path), '', { flag: 'wx' });
+      await writeFile(filePath, '', { flag: 'wx' });
     } catch {
       throw new OperationFailedError();
     }

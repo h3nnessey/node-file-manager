ee06dfa9f2510197ca0568fca4156c5f6c322838
Vitaly <vitaliy.zayakin@gmail.com>
2024-02-05 19:26:01 +0500

    feat: add rn method

--- src/services/fs-service.js
+++ src/services/fs-service.js
@@ -1,5 +1,5 @@
 import { resolve, parse } from 'node:path';
-import { writeFile, rm } from 'node:fs/promises';
+import { writeFile, rm, rename } from 'node:fs/promises';
 import { createReadStream } from 'node:fs';
 import { pipeline } from 'node:stream/promises';
 import { InvalidInputError, OperationFailedError } from '../utils/error/index.js';
@@ -48,6 +48,25 @@ export class FsService {
       const readStream = createReadStream(filePath);
 
       await pipeline(readStream, process.stdout, { end: false });
+    } catch {
+      throw new OperationFailedError();
+    }
+  }
+
+  // (do not pass pathLike as new_file_name)
+  async rn([pathToFile, newName]) {
+    const oldPath = resolve(pathToFile);
+    const oldPathDir = parse(oldPath).dir;
+
+    const newPath = resolve(oldPathDir, newName);
+    const newPathDir = parse(newPath).dir;
+
+    if (oldPathDir !== newPathDir) {
+      throw new InvalidInputError();
+    }
+
+    try {
+      await rename(oldPath, newPath);
     } catch {
       throw new OperationFailedError();
     }

96ab8d5de4b3f59a51294d64b83e53d68a1d555b
Vitaly <vitaliy.zayakin@gmail.com>
2024-02-05 17:47:29 +0500

    feat: add rm

--- src/services/fs-service.js
+++ src/services/fs-service.js
@@ -1,6 +1,7 @@
 import { resolve, parse } from 'node:path';
-import { writeFile } from 'node:fs/promises';
+import { writeFile, rm } from 'node:fs/promises';
 import { InvalidInputError, OperationFailedError } from '../utils/error/index.js';
+import { isFile } from '../utils/path/is-file.js';
 
 export class FsService {
   async add([path]) {
@@ -13,6 +14,21 @@ export class FsService {
 
     try {
       await writeFile(filePath, '', { flag: 'wx' });
+    } catch {
+      throw new OperationFailedError();
+    }
+  }
+
+  async rm([path]) {
+    const filePath = resolve(path);
+    const isPathToFile = await isFile(filePath);
+
+    if (isPathToFile === false) {
+      throw new InvalidInputError();
+    }
+
+    try {
+      await rm(filePath);
     } catch {
       throw new OperationFailedError();
     }

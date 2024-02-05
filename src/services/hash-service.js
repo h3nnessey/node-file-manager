d3a273b27130a1f7ddd2fa6500a45d0a946a1638
Vitaly <vitaliy.zayakin@gmail.com>
2024-02-06 00:03:07 +0500

    feat: add path checking to hash method

--- src/services/hash-service.js
+++ src/services/hash-service.js
@@ -2,11 +2,19 @@ import { resolve } from 'node:path';
 import { createReadStream } from 'node:fs';
 import { createHash } from 'node:crypto';
 import { pipeline } from 'node:stream/promises';
-import { OperationFailedError } from '../utils/error/index.js';
+import { InvalidInputError, OperationFailedError } from '../utils/error/index.js';
+import { isFile } from '../utils/path/is-file.js';
 
 export class HashService {
   async hash([path]) {
-    const readStream = createReadStream(resolve(path));
+    const filePath = resolve(path);
+    const isPathToFile = await isFile(filePath);
+
+    if (isPathToFile === false) {
+      throw new InvalidInputError();
+    }
+
+    const readStream = createReadStream(filePath);
     const hash = createHash('sha256');
 
     try {
